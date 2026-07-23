625cd3fb0f49e4e7a80ceb37fe1ed8c73a24281f
Simon3846 <simaosunga@gmail.com>
2026-07-23 02:02:53 -0300

    mudanca do parametro do jogador

--- uno/src/modelo/Jogador.java
+++ uno/src/modelo/Jogador.java
@@ -1,5 +1,38 @@
 package modelo;
 
-public class Jogar {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Jogador {
+
+    private String nome;
+    private ArrayList<Carta> mao; // Cartas que o jogador tem na mao
+
+    public Jogador(String nome){
+        this.nome = nome;
+        this.mao = new ArrayList<>();
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public List<Carta> getMao(){
+        return mao;
+    }
+
+    // Adicionar uma carta a mao do jogador
+
+    public void adicionarCarta(Carta carta){
+        mao.add(carta);
+    }
+
+    // Remove a carta da mao do jogador
+    public void removerCarta(Carta carta){
+        mao.remove(carta);
+    }
+
+    public boolean venceu(){
+        return mao.isEmpty();
+    }
 }
