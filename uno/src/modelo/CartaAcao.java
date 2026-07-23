9044daaf216d8102474e82f3095305b388aca111
Simon3846 <simaosunga@gmail.com>
2026-07-23 11:11:59 -0300

    inclui a o controle.Gerenciador no na class cartaAcao

--- uno/src/modelo/CartaAcao.java
+++ uno/src/modelo/CartaAcao.java
@@ -1,5 +1,18 @@
 package modelo;
 
-public class CartaAcao {
+public class CartaAcao extends Carta {
 
+    public CartaAcao(String cor, String valor){
+        super(cor,valor); // Valor será "Pular", "Inverter" ou "+2"
+    }
+
+    @Override
+    public void aplicarEfeito(Object jogo){
+        // TODO: Quando criarmos a classe do Jogo, vamos programar os efeitos aqui:
+        // Se for "Pular" -> jogo.pularVez();
+        // Se for "Inverter" -> jogo.inverterSentido();
+        // Se for "+2" -> jogo.comprarCartasParaProximo(2);
+
+        System.out.println("Aplicando o efeito da carta de ação: " + getValor());
+    }
 }
